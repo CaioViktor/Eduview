f7a791e24d6fe892fc3fc8b39495d4730befdeec
LucasFalcao <lucasfalcao.p@hotmail.com>
2016-07-05 18:35:01 -0300

    Classes novas para ajudar nas chamadas ao servidor

--- Projeto/app/src/main/java/dspm/dc/ufc/br/eduview/ServerCallsHelper.java
+++ Projeto/app/src/main/java/dspm/dc/ufc/br/eduview/ServerCallsHelper.java
@@ -16,72 +16,20 @@ import java.util.ArrayList;
 import dspm.dc.ufc.br.eduview.servercalls.*;
 
 
-public class ServerCallsHelper implements ObserverServer {
+public class ServerCallsHelper{
     private Handler handler;
     private Context callerContext;
-    private Server server;
-
 
     public ServerCallsHelper(Context mainContext){
         handler = new Handler();
         callerContext = mainContext;
-        server = new Server(mainContext);
-        server.attachObserver(this);
     }
 
     public void getEscolas(LatLng posicao,int raio, int maximo){
-        ServerCall sc = new CallEscolasProximas(posicao,raio,maximo);
+        Server server = new Server(callerContext);
+        ServerCall sc = new CallEscolasProximas(callerContext,server,posicao,raio,maximo);
+        server.attachObserver(sc);
         sc.call();
-
-        JSONObject object = new JSONObject();
-
-
-        try {
-            object.put("where","UPPER(nome) LIKE \'%ACA%\'");
-            server.POST(server.HTTP + server.HOST + server.PORT + "/listescola/" + posicao.longitude + "/" + posicao.latitude + "/"+raio+"/"+maximo, object.toString());
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        server.POST(server.HTTP + server.HOST + server.PORT + "/listescola/" + posicao.latitude + "/" + posicao.longitude+ "/"+raio+"/"+maximo, null);
-
-
     }
 
-    @Override
-    public void notifyPOST(String response) {
-
-        ArrayList<Escola> escolas = new ArrayList<>();
-        try{
-            JSONObject jsonObject = new JSONObject(response);
-            if(jsonObject.has("codigo") && jsonObject.getInt("codigo") == 1)
-                Toast.makeText(callerContext,"Ocorreu um erro interno",Toast.LENGTH_LONG);
-            else{
-                int size = jsonObject.getInt("contador");
-                for(int i = 1; i <= size; i++){
-                    Escola escola = new Escola(jsonObject.getJSONObject(i+"").toString());
-                    escolas.add(escola);
-                }
-
-            }
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        for(Escola e : escolas){
-            Log.i("LOGP",e.getBairro());
-            ((MainActivity)callerContext).marcarEscolaNoMapa(e);
-        }
-
-
-
-    }
-
-    @Override
-    public void notifyGET(String response) {
-
-    }
-
-    @Override
-    public Handler getHandler() {
-        return handler;
-    }
 }
--- Projeto/app/src/main/java/dspm/dc/ufc/br/eduview/servercalls/CallEscolasProximas.java
+++ Projeto/app/src/main/java/dspm/dc/ufc/br/eduview/servercalls/CallEscolasProximas.java
@@ -1,18 +1,73 @@
 package dspm.dc.ufc.br.eduview.servercalls;
 
 
+import android.content.Context;
 import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.model.LatLng;
 
-public class CallEscolasProximas extends ServerCall{
-    //private
-    public CallEscolasProximas(LatLng posicao, int raio, int maximo) {
-        super();
-    }
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
+
+import dspm.dc.ufc.br.eduview.Escola;
+import dspm.dc.ufc.br.eduview.MainActivity;
+import dspm.dc.ufc.br.eduview.Server;
+
+public class CallEscolasProximas extends ServerCall{
+
+    private LatLng posicao;
+    private int raio;
+    private int maximo;
+    private Handler handler = new Handler();
+    private Context callerContext;
+    private Server server;
+
+    public CallEscolasProximas(Context callerContext, Server server, LatLng posicao, int raio, int maximo) {
+        this.server = server;
+        this.callerContext = callerContext;
+        this.posicao = posicao;
+        this.raio = raio;
+        this.maximo = maximo;
+    }
+    @Override
+    public void call() {
+        JSONObject object = new JSONObject();
+        /*try {
+            object.put("where","UPPER(nome) LIKE \'%ACA%\'");
+            server.POST(server.HTTP + server.HOST + server.PORT + "/listescola/" + posicao.longitude + "/" + posicao.latitude + "/"+raio+"/"+maximo, object.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }*/
+        server.POST(server.HTTP + server.HOST + server.PORT + "/listescola/" + posicao.latitude + "/" + posicao.longitude+ "/"+raio+"/"+maximo, null);
+
+    }
     @Override
     public void notifyPOST(String response) {
+        ArrayList<Escola> escolas = new ArrayList<>();
+
+        try{
+            JSONObject jsonObject = new JSONObject(response);
+            if(jsonObject.has("codigo") && jsonObject.getInt("codigo") == 1)
+                Toast.makeText(callerContext,"Ocorreu um erro interno",Toast.LENGTH_LONG);
+            else{
+                int size = jsonObject.getInt("contador");
+                for(int i = 1; i <= size; i++){
+                    Escola escola = new Escola(jsonObject.getJSONObject(i+"").toString());
+                    escolas.add(escola);
+                }
+
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        for(Escola e : escolas){
+            Log.i("LOGP",e.getBairro());
+            ((MainActivity)callerContext).marcarEscolaNoMapa(e);
+        }
 
     }
 
@@ -22,12 +77,8 @@ public class CallEscolasProximas extends ServerCall{
     }
 
     @Override
-    public Handler getHandler() {
-        return null;
+    public Handler getHandler(){
+        return this.handler;
     }
 
-    @Override
-    public void call() {
-
-    }
 }
